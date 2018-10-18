159f31a2a632c45cb90aeea275f9479ede5e94b6
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2018-10-18 10:11:09 +0530

    Got max 20 points for alphabet_rangoli.py

---
+++ alphabet_rangoli.py
@@ -0,0 +1,16 @@
+import string
+
+def print_rangoli(n):
+    
+    alpha = string.ascii_lowercase
+    L = []
+
+    for i in range(n):
+        s = '-'.join(alpha[i:n])
+        L.append((s[:0:-1] + s).center(4*n - 3, '-'))
+
+    print('\n'.join(L[:0:-1]+L))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
