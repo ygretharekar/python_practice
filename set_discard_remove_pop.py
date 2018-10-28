1aef7e78e66424cd5fceb2a49bbf8ee8cf4fc4a5
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2018-10-28 23:18:17 +0530

    Got max 10 points for set_discard_remove_pop.py

---
+++ set_discard_remove_pop.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    s = set(map(int, input().split()))
+
+    for i in range(int(input())):
+        eval('s.{0}({1})'.format(*input().split() + ['']))
+
+
+print(sum(s))
